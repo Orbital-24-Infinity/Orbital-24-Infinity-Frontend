8697878be71bc883a28254d3d56c61ea81686cb4
neohengkai <neohengkai2002@gmail.com>
2024-06-01 23:59:37 +0800

    login done

---
+++ app/login/Authenticate.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Authenticate = () => {
+    return <Image src="/images/web_dark_rd_SI@4x.png" width={150} height={45} alt="google button" />;
+};
+
+export default Authenticate;
