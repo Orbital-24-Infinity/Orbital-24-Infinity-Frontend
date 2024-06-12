5244266721157902e2f7862013bbaf6a1c976d22
unknown <2poh.junkang@gmail.com>
2024-06-12 23:27:01 +0800

    dashboard task ui with fake data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,27 +2,106 @@ import "../../styles/Dashboard.sass";
 
 import React from "react";
 
+import Icon, { Icons } from "@/components/icons";
 import NavbarComponent from "@/components/navbar";
 
 import AuthWrapper from "../../components/firebase-auth/AuthWrapper";
 
+interface ITopics {
+  topicID: number;
+  topicName: string;
+  lastModified: string;
+  status: string;
+}
+
+const getStatusColour = (status: string) => {
+  switch (status) {
+    case "Generating...":
+      return "#FF7B00";
+    case "Completed":
+      return "#00ff00";
+    case "Failed":
+      return "#ff0000";
+    default:
+      return "#EEFF00";
+  }
+};
+
 const DashboardComponent = () => {
+  const topics: ITopics[] = [
+    {
+      topicName: "CS2030S Finals Practice",
+      topicID: 1,
+      lastModified: "12th June 2024",
+      status: "Generating...",
+    },
+    {
+      topicName: "CS2040S Mid-Term Practice",
+      topicID: 3,
+      lastModified: "23rd May 2024",
+      status: "Attempting (5/10)",
+    },
+    {
+      topicName: "CS2030S Mid-Term Practice",
+      topicID: 2,
+      lastModified: "3rd May 2024",
+      status: "Completed",
+    },
+    {
+      topicName: "CS2030S Mid-Term Practice",
+      topicID: 2,
+      lastModified: "3rd May 2024",
+      status: "Completed",
+    },
+    {
+      topicName: "CS2030S Mid-Term Practice",
+      topicID: 2,
+      lastModified: "3rd May 2024",
+      status: "Completed",
+    },
+  ];
+
   return (
     <div className="dashboardComponent">
-      <h1 className="topicHeader">Your Topics</h1>
+      <div className="topicMainHeaderWrapper">
+        <h1 className="topicMainHeader">Your Topics</h1>
+      </div>
 
-      <button className="newTopic">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 -960 960 960"
-          width="24px"
-          fill="#e8eaed"
-        >
-          <path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z" />
-        </svg>
-        <p>New Topic</p>
-      </button>
+      <div className="topics">
+        {topics.map((topic) => {
+          return (
+            <div key={topic.topicID} className="topic">
+              <div className="topicHeader">
+                <input value={topic.topicName} className="topicName" />
+                <Icon
+                  type={Icons.Delete}
+                  colour="#ff0000"
+                  hoverEffects
+                  className="topicDelete"
+                />
+              </div>
+
+              <p style={{ color: getStatusColour(topic.status) }}>
+                {topic.status}
+              </p>
+
+              <div className="topicFooter">
+                <div></div>
+                <p className="topicLastModified">
+                  Last Modified: {topic.lastModified}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="newTopicWrapper">
+        <button className="newTopic">
+          <Icon type={Icons.Add} />
+          <p>New Topic</p>
+        </button>
+      </div>
     </div>
   );
 };
